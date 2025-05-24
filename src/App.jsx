4cbb5e13653b20456b5cec0b62e05269a77605bd
Akhil-02R <akhil0202r@gmail.com>
2025-05-24 12:25:31 +0530

    update 1.1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react"
+import Header from "./Components/Header"
+import CardArea from "./Components/CardArea"
+
+const App = () => {
+  return(
+    <Fragment>
+      <Header />
+      <CardArea />
+    </Fragment>
+  )
+}
+
+export default App
---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import './Style/header.css';
+
+const Header = () => {
+  return(
+      <div className="header">
+        <img src="src/assets/image.png" alt="Globe" />
+        <p>My Travel Journey.</p>
+      </div>
+  )
+}
+
+export default Header;
